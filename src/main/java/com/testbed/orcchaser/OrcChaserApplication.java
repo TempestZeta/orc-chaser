5d1082db7b50affde569644e4be4fe62da836d49
TempestZeta <gjstkd1230@gmail.com>
2021-10-17 02:12:28 +0900

    Initial commit

---
+++ src/main/java/com/testbed/orcchaser/OrcChaserApplication.java
@@ -0,0 +1,13 @@
+package com.testbed.orcchaser;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrcChaserApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OrcChaserApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/testbed/orcchaser/OrcChaserApplicationTests.java
@@ -0,0 +1,13 @@
+package com.testbed.orcchaser;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrcChaserApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
